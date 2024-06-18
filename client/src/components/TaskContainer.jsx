dc49b86ca019fe648554ebed8e61e97d2cd7ea44
hasanbadalli <hasanoaga2005@gmail.com>
2024-06-18 23:26:22 +0400

    CRUD COMPLETED

--- client/src/components/TaskContainer.jsx
+++ client/src/components/TaskContainer.jsx
@@ -15,6 +15,29 @@ const TaskContainer = ({ tasks, setTasks }) => {
     );
   };
 
+  const taskDelete = async (id) => {
+    try {
+      const response = await fetch("http://localhost:3000/taskapi/delete", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: id }),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+  
+      await response.json();
+  
+      tasks = tasks.filter((task) => task.id !== id);
+      setTasks(tasks)
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
+  };
+
   return (
     <div className="tasks-container">
       <ul className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
@@ -28,7 +51,7 @@ const TaskContainer = ({ tasks, setTasks }) => {
             <li key={task.id} style={{textDecoration: task.completed ? "line-through" : "none"}}>
               <span onClick={()=>toggleCompleted(task.id , !task.completed)}>{task.text}</span>
             </li>
-            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+            <button onClick={()=>taskDelete(task.id)} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
               <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 Delete
               </span>
--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const {taskAdder, taskComplete, taskGetter} = require("../controllers/taskController")
+const {taskAdder, taskComplete, taskGetter, taskDelete} = require("../controllers/taskController")
 
 router.get("/", taskGetter)
 router.post("/post", taskAdder)
 router.put("/put", taskComplete)
+router.delete("/delete", taskDelete)
 
 module.exports = router
